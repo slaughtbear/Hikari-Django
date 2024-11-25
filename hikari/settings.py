0d7409724ab94f6c6ae4a87f8dbc0f113f60434e
slaughtbear <ivanaguirre368@gmail.com>
2024-11-24 18:15:25 -0600

    Add: Configuraciones de TailwindCSS

--- hikari/settings.py
+++ hikari/settings.py
@@ -37,8 +37,19 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'tailwind', # TailwindCSS
+    'theme', # TailwindCSS app
+    'django_browser_reload', # Recarga automática para páginas
     'authentication', # Autenticación de usuarios
 ]
+
+TAILWIND_APP_NAME = 'theme'
+
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
+
+NPM_BIN_PATH = r"C:\Program Files\nodejs\npm.cmd"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -48,6 +59,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django_browser_reload.middleware.BrowserReloadMiddleware', # Recarga automática para páginas
 ]
 
 ROOT_URLCONF = 'hikari.urls'
