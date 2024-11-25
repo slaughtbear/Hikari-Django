72709bc64d521039487c488ff1354d6fdc7142d1
slaughtbear <ivanaguirre368@gmail.com>
2024-11-24 18:16:26 -0600

    Add: Middleware para recarga automática de páginas

--- hikari/urls.py
+++ hikari/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('auth/', include('authentication.urls'))
+    path("__reload__/", include("django_browser_reload.urls")), # Middleware para recarga automática de páginas
+    path('auth/', include('authentication.urls')),
 ]
