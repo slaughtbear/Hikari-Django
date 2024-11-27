9121471f0ff957c0e686336d2f4a5f5a646da7be
slaughtbear <ivanaguirre368@gmail.com>
2024-11-27 13:01:23 -0600

    Add: Url signin/

--- authentication/urls.py
+++ authentication/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('signup/', views.signup, name='signup'),
+    path('signin/', views.signin, name='signin'),
     path('prueba/', views.prueba, name='prueba'),
 ]
