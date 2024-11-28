225b92ec06f3a62aa145f22537dc16b8ae27a385
slaughtbear <ivanaguirre368@gmail.com>
2024-11-28 13:32:26 -0600

    Add: Urls de la app landing

--- hikari/urls.py
+++ hikari/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("__reload__/", include("django_browser_reload.urls")), # Middleware para recarga automática de páginas
     path('auth/', include('authentication.urls')),
+    path('landing/', include('landing.urls'))
 ]
